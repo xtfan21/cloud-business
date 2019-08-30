073c3c4fe6473d87f832632cecbf70d8a472af9d
kang.han <kang.han@shuyun.com>
2019-08-30 18:30:22 +0800

    添加cc-menu组件

--- src/index.js
+++ src/index.js
@@ -5,5 +5,9 @@ if( typeof window === 'undefined' ) {
 	console.warn('cloud-business@0.0.1 仅支持在浏览器环境进行使用!');
 }
 
+export { default as AreaSelector } from './area-selector';
+
+export { default as CcMenu } from './cc-menu';
+
 export { default as Sms } from './sms';
-export { default as areaSelector } from './area-selector';
+
