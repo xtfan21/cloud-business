c6708a30385eac56e69b4daf4123008121c7cfed
fang.yang <yangfang2028@126.com>
2020-01-11 22:30:28 +0800

    :arrow_up: 升级cloud-react到0.0.27版本

--- demos/index.js
+++ demos/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import 'cloud-react/cloud-react.css';
+
 import App from './app';
+
 
 const rootElement = document.getElementById('root');
 
